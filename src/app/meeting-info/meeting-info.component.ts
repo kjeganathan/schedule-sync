694df3b3fb5fcd78b2dde0bb1aec11efb01f8b3e
Rinija Raja <73535868+rinraja@users.noreply.github.com>
2021-11-11 23:58:57 -0500

    meeting-info button addition

--- src/app/meeting-info/meeting-info.component.ts
+++ src/app/meeting-info/meeting-info.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meeting-info.component.css']
 })
 export class MeetingInfoComponent implements OnInit {
-
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
   constructor() { }
 
   ngOnInit(): void {
