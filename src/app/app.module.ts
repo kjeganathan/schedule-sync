0cb88cbc76c0119f269eb669c316712c236d2726
awatterson22 <abbywatterson19@yahoo.com>
2021-11-16 14:46:45 -0500

    adding google login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatButtonModule } from '@angular/material/button';
-import { SocialLoginModule, SocialAuthServiceConfig } from "angularx-social-login";
-import { GoogleLoginProvider } from "angularx-social-login";
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CalendarComponent } from './calendar/calendar.component';
@@ -15,6 +13,13 @@ import { MeetingsComponent } from './meetings/meetings.component';
 import { SchedulingComponent } from './scheduling/scheduling.component';
 import { MeetingInfoComponent } from './meeting-info/meeting-info.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+import { environment } from 'src/environments/environment';
+import { AuthenticationService } from './authentication.service';
 
 @NgModule({
   declarations: [
@@ -34,24 +39,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatIconModule,
     MatChipsModule,
     MatButtonModule,
-    SocialLoginModule
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
-  providers: [
-    {
-      provide: "SocialAuthServiceConfig",
-      useValue: {
-        autoLogin: true,
-        providers: [
-          {
-            id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider(
-              '500421331480-8o0r6v4m948lgr10vd0i6par63e9vsu5.apps.googleusercontent.com'
-            )
-          }
-        ]
-      } as SocialAuthServiceConfig
-    }
-  ],
+  providers: [AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import firebase from 'firebase/compat/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  public user: any;
+
+  constructor(private router: Router, public auth: AngularFireAuth) {
+    this.auth.authState.subscribe(user => {
+      if (user) {
+        this.user = user;
+      } else {
+        this.user = null;
+      }
+    })
+   }
+  
+  async login() {
+    await this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    this.router.navigate(['/my-calendar']);
+  }
+  logout() {
+    this.auth.signOut();
+    this.router.navigate(['/login']);
+  }
 }
