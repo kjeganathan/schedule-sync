fc1e0c5d804b54a87fbaa0b4de847d378e5cab89
awatterson22 <abbywatterson19@yahoo.com>
2021-11-18 14:33:55 -0500

    adding package.json and index.js database tables

--- index.js
+++ index.js
@@ -28,3 +28,26 @@ express()
     res.sendFile(path.join(__dirname + "/public/src/html/login.html"));
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+/* * * * * * * * * * * * * * * * * * * * * *  
+ *             DATABASE TABLES             *
+ * * * * * * * * * * * * * * * * * * * * * * 
+TABLE users( 
+    user_id SERIAL PRIMARY KEY, 
+    first_name TEXT NOT NULL, 
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    meetings TEXT NOT NULL ---> string of json array
+    tentative_meetings TEXT NOT NULL ---> string of json array
+);
+TABLE meetings( 
+    meeting_id SERIAL PRIMARY KEY, 
+    title TEXT NOT NULL, 
+    date DATE NOT NULL,
+    start TIME NOT NULL,
+    end TIME NOT NULL,
+    location TEXT NOT NULL,
+    description TEXT NOT NULL,
+    attendees TEXT NOT NULL  ---> string of json array
+);
+*/
