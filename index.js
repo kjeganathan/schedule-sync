f5775f4de47fe198ce56e7e83ed1da4995614757
DanGithub200 <89949207+DanGithub200@users.noreply.github.com>
2021-11-19 09:20:45 -0500

    Update index.js
    check log, initialize user, create meeting

--- index.js
+++ index.js
@@ -51,3 +51,69 @@ TABLE meetings(
     attendees TEXT NOT NULL  ---> string of json array
 );
 */
+
+const database = require('./data.json');
+import { getAuth } from "firebase/auth";
+const user = getAuth().currentUser;
+
+app.get('/check_if_logged_in', async (req, res, next) => {
+  if (user) {//user signed in
+    next();
+  } else {
+    res.sendFile(path.join(__dirname + "/public/src/html/login.html"));
+  }
+  
+});
+
+//intialize user
+app.post('/new-user', async (req, res) => {
+  req.on('end', () => {
+    const data = JSON.parse(body);
+    database.wordScores.push({
+        user_id: 1, //dunno what the plan for this is | fill it in when known
+        first_name: data.first_name,
+        last_name: data.last_name,
+        email: user.email,
+        meetings: [],
+        tentative_meetings: []
+        
+        //not included in the commented out list above, but we can consider them
+        /*
+        ,
+        user_photo: user.photoURL,
+        notifications: [] //meetings
+        */
+    });
+    
+    writeFile("./data.json", JSON.stringify(database), err => {
+        if (err) {
+            console.err(err);
+        } else res.end();
+    });
+});
+
+});
+
+//create meeting
+app.post('/create-meeting', async (req, res) => {
+  req.on('end', () => {
+    const data = JSON.parse(body);
+    database.wordScores.push({
+      meeting_id: 1, //dunno what the plan for this is | fill it in when known
+      title: data.title,
+      date: data.date,
+      start_time: data.start_time,
+      end_time: data.end_time,
+      location: data.location,
+      description: data.description,
+      attendees: data.attendees,
+    });
+    
+    writeFile("./data.json", JSON.stringify(database), err => {
+        if (err) {
+            console.err(err);
+        } else res.end();
+    });
+});
+
+});
