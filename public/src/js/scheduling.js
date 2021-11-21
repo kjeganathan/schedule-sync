82173c150927b0d5f219c2c7f6e9ef0014833e44
Janvi Tanniru <janvitanniru@gmail.com>
2021-11-20 19:34:49 -0500

    scheduling form html and js working

--- public/src/js/scheduling.js
+++ public/src/js/scheduling.js
@@ -1,17 +1,25 @@
 'use strict';
 
 window.addEventListener("load", async function () {
-  const scheduleButton = document.getElementById('schedule');
+  const scheduleButton = document.getElementById('Schedule');
   scheduleButton.addEventListener('click', async () => {
     const addMeetingTitle = document.getElementById('meetingTitle').value; 
     const meetingDate = document.getElementById('meetingDate').value;
     const startTime = document.getElementById('startTime').value; 
     const endTime = document.getElementById('endTime').value; 
-    const location = document.getElementById('Location').value; 
-    const description = document.getElementById('Description').value;
-    const attendeeNumber = document.getElementById('attendeeNumber').value;
+    const description = document.getElementById('description').value;
+    const attendeeEmails = document.getElementById('attendeeEmail').value;
+    const attendeeEmailsArray = attendeeEmails.split(",");
+    
+    let locationValue = ""; 
+    if(document.getElementById('remoteCheckbox').checked){
+      locationValue = document.getElementById('remote').value;
+    }
+    if(document.getElementById('inPersonCheckbox').checked){
+      locationValue = document.getElementById('inPerson').value;
+    }
 
-    if(addMeetingTitle === '' || meetingDate === '' || startTime === '' || endTime === '' || location === '' || description === '' || attendeeNumber ===''){
+    if(addMeetingTitle === '' || meetingDate === '' || startTime === '' || endTime === '' || description === '' || attendeeEmails ==='' || remote === '' || inPerson ===''){
       alert('Required information is missing.');
     }
     else {
@@ -25,9 +33,11 @@ window.addEventListener("load", async function () {
             meetingDate: meetingDate,
             startTime: startTime,
             endTime: endTime,
-            location:location,
             description: description,
-            attendeeNumber: attendeeNumber
+            attendeeEmails: attendeeEmails,
+            attendeeEmailsArray: attendeeEmailsArray,
+            location: locationValue
+
         })
       });
 
