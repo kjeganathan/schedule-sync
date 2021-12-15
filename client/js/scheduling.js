6a0977b200d65bd60e504d1475dbc70aab209765
awatterson22 <abbywatterson19@yahoo.com>
2021-12-14 22:21:50 -0500

    Add free/busy to app

--- client/js/scheduling.js
+++ client/js/scheduling.js
@@ -54,7 +54,7 @@ window.addEventListener("load", async function () {
   suggestionDate.addEventListener("input", async () => {
     let dateMin = new Date(suggestionDate.value);
     let dateMax = new Date(suggestionDate.value);
-    dateMax.setDate(dateMax.getDate() + 7);
+    dateMax.setDate(dateMax.getDate() + 1);
     meetingSuggestions(dateMin, dateMax);
   });
 });
@@ -206,38 +206,42 @@ async function meetingSuggestions(dateMin, dateMax) {
     },
   })
     .then((response) => response.text())
-    .then((result) => {
-      console.log(result);
-      let suggestionHtml = "";
+    .then((results) => {
+      let data = JSON.parse(results);
+      if (data.length) {
+        let availabilityHtml = "";
+        data.sort(function (a, b) {
+          return a.start.localeCompare(b.start);
+        });
+        data.forEach((result) => {
+          let suggestion = `<div class="card">
+          <div class="card-horizontal">
+            <div class="img-square-wrapper">
+              <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" id="calendar-icon" class="bi bi-calendar-fill" viewBox="0 0 16 16">
+                   <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5h16V4H0V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5z"/>
+                </svg>
+            </div>
+            <div>
+              <h2 class="card-title">Busy</h2>
+              <div class="card-text" id="start-time">Start time: ${new Date(
+                result.start
+              ).toLocaleTimeString()}</div>
+              <div class="card-text" id="end-time">End Time: ${new Date(
+                result.end
+              ).toLocaleTimeString()}</div>
+            </div>
+          </div>
+        </div>`;
+          availabilityHtml += suggestion;
+        });
+        document.getElementById("availability").innerHTML = availabilityHtml;
+      } else {
+        document.getElementById("no-availability").innerHTML =
+          "Currently available all day.";
+        document.getElementById("availability").innerHTML = "";
+      }
     })
     .catch((error) => {
       console.log(error);
     });
 }
-
-let suggestion = `<div class="card">
-          <div class="card-horizontal">
-            <div class="img-square-wrapper">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
-                fill="currentColor"
-                class="bi bi-alarm-fill"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zm2.5 5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527z"
-                />
-              </svg>
-            </div>
-            <div>
-              <h2 class="card-title">Time Block Suggestion 2</h2>
-              <div class="card-text" id="start-time">Start time: </div>
-              <div class="card-text" id="end-time">End Time: </div>
-            </div>
-          </div>
-          <div>
-            <button type="button" class="btn btn-light detail">Select</button>
-          </div>
-        </div>`;
